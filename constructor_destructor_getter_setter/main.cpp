09b7e1afc1d89adf4f9cdfb48607b64fb2d01e00
DungxLas <phamjhungfdungx@gmail.com>
2022-09-27 21:06:59 +0700

    Initial Commit

---
+++ constructor_destructor_getter_setter/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  constructor_destructor_getter_setter
+//
+//  Created by PHẠM HÙNG DŨNG on 27/09/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
